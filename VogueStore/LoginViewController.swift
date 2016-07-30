d7d4b16a921b1949b8ad535290af92ff0f031c3e
Debaprio Banik <dbanik@apple.com>
2016-07-29 20:11:31 -0700

    First checkin

---
+++ VogueStore/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  VogueStore
+//
+//  Created by Debaprio Banik on 7/29/16.
+//  Copyright © 2016 Debaprio Banik. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ VogueStore/StoreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StoreViewController.swift
+//  VogueStore
+//
+//  Created by Debaprio Banik on 7/29/16.
+//  Copyright © 2016 Debaprio Banik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StoreViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var offerWidth: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
